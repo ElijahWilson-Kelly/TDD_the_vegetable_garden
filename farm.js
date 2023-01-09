022ac5775c754afabd5d21f4d560a05cbba08819
Elijah Wilson-Kelly <elijahwilsonkelly@gmail.com>
2023-01-09 07:45:43 +0100

    added yield functions

--- farm.js
+++ farm.js
@@ -1,2 +1,19 @@
+// Helper Functions
+const getYieldForPlant = (plant) => plant.yield;
+
+const getYieldForCrop = ({ crop, numCrops }) => crop.yield * numCrops;
+
+const getTotalYield = function ({ crops }) {
+  let result = 0;
+  crops.forEach((crop) => {
+    result += getYieldForCrop(crop);
+  });
+  return result;
+};
+
 // Exporting functions
-module.exports = {};
+module.exports = {
+  getYieldForPlant,
+  getYieldForCrop,
+  getTotalYield,
+};
